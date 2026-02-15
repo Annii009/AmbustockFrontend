43ff6aa3fa5014f6d2b68cd46b4e1f61f5f8eed0
Annii009 <anaheralmudi@gmail.com>
2026-02-15 21:06:01 +0100

    añadido dockercompose y arregladas cosas de diseño

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string
+  // Añade más variables aquí si necesitas
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
