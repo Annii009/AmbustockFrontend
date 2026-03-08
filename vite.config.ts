1ca24daaa7bc8611d532e8605afb50149c68ecb6
Annii009 <anaheralmudi@gmail.com>
2026-03-08 21:35:45 +0100

    quitado vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,10 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
   ],
   resolve: {
     alias: {
@@ -25,6 +22,7 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173,
     strictPort: true,
+    allowedHosts: true,
     watch: {
       usePolling: true
     }
